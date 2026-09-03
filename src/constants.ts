85f31e6573dfedf79eabfd602793f1f35d563ada
Thomerson Roncally Araújo Teixeira <thomersonroncally@outlook.com>
2026-09-03 19:15:57 -0400

    chore: prepare storybook brands addon package

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,4 @@
-export const ADDON_ID = 'my-addon';
+export const ADDON_ID = 'storybook-brands-addon';
 export const TOOL_ID = `${ADDON_ID}/tool`;
-export const PANEL_ID = `${ADDON_ID}/panel`;
-export const TAB_ID = `${ADDON_ID}/tab`;
-export const KEY = `my-addon`;
-
-export const EVENTS = {
-  RESULT: `${ADDON_ID}/result`,
-  REQUEST: `${ADDON_ID}/request`,
-};
+export const BRAND_GLOBAL = 'brand';
+export const BRANDS_PARAMETER = 'brands';
